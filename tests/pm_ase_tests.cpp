1dbdeba68fec9f3390a4d9534b64b38a040b142a
isRyven <ryven.mt@gmail.com>
2018-07-11 15:13:18 +0300

    add ase tests

---
+++ tests/pm_ase_tests.cpp
@@ -0,0 +1,44 @@
+#include "catch.hpp"
+#include "picomodel.h"
+#include "utils.h"
+
+TEST_CASE("Should load simple ase") {
+    picoModel_t *model = PicoLoadModel("../tests/assets/model.ase", 0);
+    REQUIRE(model != NULL);
+    PicoFreeModel(model);
+}
+
+TEST_CASE("Should correctly parse simple ase") {
+    picoModel_t *model = PicoLoadModel("../tests/assets/model.ase", 0);
+    int numSurfaces = PicoGetModelNumSurfaces(model);
+    REQUIRE(numSurfaces == 1);
+    picoSurface_t *surface = PicoGetModelSurface(model, 0);
+    int numShaders = PicoGetModelNumShaders(model);
+    REQUIRE(numShaders == 1);
+    picoShader_t *shader = PicoGetSurfaceShader(surface);
+    REQUIRE(std::string("myshader_1") == PicoGetShaderName(shader));
+    int numVerts = PicoGetSurfaceNumVertexes(surface);
+    REQUIRE(numVerts == 5);
+    int numInds = PicoGetSurfaceNumIndexes(surface);
+    REQUIRE(numInds == 6);
+
+    picoVec3_t verts[] = {
+        { -1.f, -1.f, 0.f },
+        { -1.f, 1.f, 0.f },
+        { 1.f, -1.f, 0.f },
+        { -1.f, 1.f, 0.f },
+        { 1.f, 1.f, 0.f }
+    };
+    for (int i = 0; i < numVerts; i++) {
+        REQUIRE(comparevec<3>(PicoGetSurfaceXYZ(surface, i), verts[i]));
+    }
+
+    picoVec3_t cvn{ 0.0f, 0.0f, 1.0f };
+    picoVec2_t sts[] = { { 0.f, 1.f },{ 0.f, 0.f },{ 1.f, 1.f },{ 0.f, 0.f }, { 1.f, 0.f } };
+    for (int n = 0; n < numVerts; n++) {
+        REQUIRE(comparevec_approx<3>(PicoGetSurfaceNormal(surface, n), cvn, 0.001f)); // normal
+        REQUIRE(comparevec<2>(PicoGetSurfaceST(surface, 0, n), sts[n])); // st
+    }
+
+    PicoFreeModel(model);
+}
